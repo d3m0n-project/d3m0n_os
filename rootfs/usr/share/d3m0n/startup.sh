dc408a6d64d43c735f3cd506d14ea73ac8641ee8
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2023-08-30 18:52:07 +0200

    Add files via upload

--- rootfs/usr/share/d3m0n/startup.sh
+++ rootfs/usr/share/d3m0n/startup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash 
 # d3m0n_os startup object (executed in /etc/rc.local)
 
+xrandr --output Screen1 --mode 640x480
+
+
 printf "\n"
 printf "\n"
 printf "[  \033[0;32mOK\033[0m  ] d3m0n started successfully\n"
 printf "\n"
 printf "\n"
 
+
+
 bash /usr/share/d3m0n/display/run.sh
+mono /usr/share/d3m0n/apps/d3m0n.exe
 
 exit 0
