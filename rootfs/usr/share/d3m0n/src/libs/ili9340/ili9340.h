2927cabc58d07474a00a863114390de69dcb5965
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-02-02 18:48:36 +0100

    Add files via upload

---
+++ rootfs/usr/share/d3m0n/src/libs/ili9340/ili9340.h
@@ -0,0 +1,48 @@
+#include "fontx.h"
+
+#define RED             0xf800
+#define GREEN           0x07e0
+#define BLUE            0x001f
+#define BLACK           0x0000
+#define WHITE           0xffff
+#define GRAY            0x8c51
+#define YELLOW          0xFFE0
+#define CYAN            0x07FF
+#define PURPLE          0xF81F
+
+#define DIRECTION0      0
+#define DIRECTION90     1
+#define DIRECTION180    2
+#define DIRECTION270    3
+
+#define rgb565(r, g, b) (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3))
+
+void lcdWriteCommandByte(uint8_t c);
+void lcdWriteDataByte(uint8_t c);
+void lcdWriteDataWord(uint16_t w);
+void lcdWriteColor(uint16_t color, size_t size);
+
+void lcdInit(int width, int height, int offsetx, int offsety);
+void lcdReset(void);
+void lcdSetup(void);
+void lcdDrawPixel(uint16_t x, uint16_t y, uint16_t color);
+void lcdDrawFillRect(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+void lcdDisplayOff(void);
+void lcdDisplayOn(void);
+void lcdInversionOn(void);
+void lcdFillScreen(uint16_t color);
+void lcdDrawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+void lcdDrawRect(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+void lcdDrawCircle(uint16_t x0, uint16_t y0, uint16_t r, uint16_t color);
+void lcdDrawFillCircle(uint16_t x0, uint16_t y0, uint16_t r, uint16_t color);
+void lcdDrawRoundRect(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t r, uint16_t color);
+void lcdDrawArrow(uint16_t x0,uint16_t y0,uint16_t x1,uint16_t y1,uint16_t w,uint16_t color);
+void lcdDrawFillArrow(uint16_t x0,uint16_t y0,uint16_t x1,uint16_t y1,uint16_t w,uint16_t color);
+int lcdDrawSJISChar(FontxFile *fx, uint16_t x,uint16_t y,uint16_t sjis,uint16_t color);
+int lcdDrawUTF8Char(FontxFile *fx, uint16_t x,uint16_t y,uint8_t *utf8,uint16_t color);
+int lcdDrawUTF8String(FontxFile *fx, uint16_t x,uint16_t y,unsigned char *utfs,uint16_t color);
+void lcdSetFontDirection(uint16_t);
+void lcdSetFontFill(uint16_t color);
+void lcdUnsetFontFill(void);
+void lcdSetFontUnderLine(uint16_t color);
+void lcdUnsetFontUnderLine(void);
