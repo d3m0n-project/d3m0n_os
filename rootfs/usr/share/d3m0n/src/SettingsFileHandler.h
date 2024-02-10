797d554e11e29a7540529b19be6a508c6c23f61b
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-02-10 23:44:05 +0100

    Delete rootfs/usr/share/d3m0n/src directory

--- rootfs/usr/share/d3m0n/src/SettingsFileHandler.h
+++
@@ -1,8 +0,0 @@
-// functions.h
-#ifndef SETTINGSFILEHANDLER_H
-#define SETTINGSFILEHANDLER_H
-
-char* getSetting(char *name, char *path);
-void writeSetting(char *name, char *value, char *path);
-
-#endif
--- rootfs/usr/share/d3m0n/src/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-gcc *.cpp *.c -Ilibs/ili9340/ -Wall -std=c99 -o d3m0n
-
-chmod 777 ./d3m0n
-
-./d3m0n
